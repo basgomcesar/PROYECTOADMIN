aafe32512369ccd6f90d659a5541413af78b3fa6
basgomcesar <cesarbasiliogomez@gmail.com>
2024-11-27 11:33:28 -0600

    agregar contador

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.getElementById("miBoton").addEventListener("click", presionarBoton);
+const contador = document.getElementById("contador");
 
 function presionarBoton() {
-  alert("¡Hola, mundo!");
+    let valor = parseInt(contador.innerHTML);
+    valor++;
+    contador.innerHTML = valor;
 }
 
